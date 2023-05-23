15b4b10f36381161d9aeaf6ef4a976772a794d8b
frances-ux <francesmarquez94@gmail.com>
2023-05-23 15:22:56 -0500

    completed sections 1 and 2 of homework 15

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 
 // mongoose.connect('mongodb://localhost:27017/codesquadComics', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
 //     if (!err) {
@@ -12,5 +14,16 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err))
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/codesquadComics')
+    await mongoose.connect('mongodb+srv://francesmarquez94:aynFa23L1fu8CBoh@cluster1.s01nuoo.mongodb.net/codesquadComics?retryWrites=true&w=majority')
 }
+// need help refactoring the code above  
+
+// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//   if(!err) {
+//     console.log("Successful connection with MongoDB Server");  
+//   }
+//   else {
+//       console.log("Error with MongoDB's connectivity");
+//       console.log(err)
+//   }
+// });
